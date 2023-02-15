a5f7e4d12cfb60056b6242cee415c5bc09543dee
Thiago Sinji Shimada Ramos <shimada+github@nikkei.one>
2023-02-15 10:23:49 +0900

    fix

--- app/code/SwiftOtter/OrderExport/Console/Command/OrderExport.php
+++ app/code/SwiftOtter/OrderExport/Console/Command/OrderExport.php
@@ -3,7 +3,9 @@
 namespace SwiftOtter\OrderExport\Console\Command;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class OrderExport extends Command
@@ -36,7 +38,7 @@ class OrderExport extends Command
                 InputOption::VALUE_OPTIONAL,
                 'Merchant notes'
             );
-    }   
+    }
 
     /**
      * @inheritdoc
