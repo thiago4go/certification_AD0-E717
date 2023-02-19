4d93a81c9494894371c4125b21a235be76c6c2aa
Thiago Sinji Shimada Ramos <shimada+github@nikkei.one>
2023-02-19 15:29:36 +0900

    step 2.3.2 completed

--- app/code/SwiftOtter/OrderExport/Action/CollectOrderData.php
+++ app/code/SwiftOtter/OrderExport/Action/CollectOrderData.php
@@ -3,16 +3,22 @@ declare(strict_types=1);
 namespace SwiftOtter\OrderExport\Action;
 
 use Magento\Sales\Api\OrderRepositoryInterface;
+use SwiftOtter\OrderExport\Api\OrderDataCollectorInterface;
 use SwiftOtter\OrderExport\Model\HeaderData;
 
 class CollectOrderData
 {
+    /** @var OrderRepositoryInterface */
     private OrderRepositoryInterface $orderRepository;
+    /** @var OrderDataCollectorInterface[] */
+    private array $collectors;
 
     public function __construct(
-        OrderRepositoryInterface $orderRepository
+        OrderRepositoryInterface $orderRepository,
+        array $collectors = []
     ) {
         $this->orderRepository = $orderRepository;
+        $this->collectors = $collectors;
     }
 
     public function execute(int $orderId, HeaderData $headerData): array
@@ -20,7 +26,9 @@ class CollectOrderData
         $order = $this->orderRepository->get($orderId);
 
         $output = [];
-        // TODO append the output
+        foreach ($this->collectors as $collector) {
+         $output = array_merge_recursive($output, $collector->collect($order, $headerData));
+        }
         return $output;
     }
 }
---
+++ app/code/SwiftOtter/OrderExport/Api/OrderDataCollectorInterface.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+namespace SwiftOtter\OrderExport\Api;
+
+use Magento\Sales\Api\Data\OrderInterface;
+use SwiftOtter\OrderExport\Model\HeaderData;
+
+interface OrderDataCollectorInterface
+{
+    public function collect(
+        OrderInterface $order,
+        HeaderData $headerData
+    ): array;
+
+}
